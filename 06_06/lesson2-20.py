0b225fe56df82da03a8e01c9905182656672b40f
jimmytseng7280 <jimmytseng7280@gmail.com>
2026-06-06 14:17:14 +0800

    06_06

---
+++ 06_06/lesson2-20.py
@@ -0,0 +1,33 @@
+import requests
+from requests import response #可加入此行(亦可不需要加)，讓 response 變成一個 typehint，方便以後看到資料型態
+# Response灰色表示沒使用
+def main():
+
+    url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+
+    print("下載台北市YOUBIKE即時資訊")
+
+    response = requests.get(url)
+    #response:requests.Response = requests.get(url)
+    print(type(response))
+    # typehint 是一個變數，裡面存放了 url 的資料型態
+    # url:str = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+    # 方便以後看到資料型態，typehint不一定要寫，但建議寫上去，讓程式碼更清楚
+
+    if response.status_code == 200: # 200 代表是網址連結成功的意思
+        #
+        data = response.json()  # json() 是respones的實體方法，會把 response 轉成 json 格式的資料
+                                # json(**kwargs.Any) -> Any，**表示沒有限定參數的型態，Any表示回傳的資料型態也沒有限定
+        #data:list = response.json()
+        print("下載成功")
+        print(type(data))       # type(data)) 是一個內建函式，會回傳 data 的資料型態
+                                # type() 是一個內建函式，會回傳資料的型態，data 是一個 list，所以 type(data) 會回傳 list
+        print(len(data))        # len(data)) 是一個內建函式，會回傳 data 的長度
+                                # len() 是一個內建函式，會回傳資料的長度，data 是一個 list，所以 len(data) 會回傳 list 的長度
+        print(data[0])          # data[0] 是一個 list 的第一筆資料，因為 data 是一個 list，所以可以用 index 來取出資料
+    else:                           # 其他狀態 或是 以404 代表網址連結失敗的意思
+        print("下載失敗")
+        print(response.status_code)
+
+if __name__ == '__main__':
+    main()
---
+++ 06_06/lesson2-20自動註解.py
@@ -0,0 +1,41 @@
+# 導入requests庫用於發送HTTP請求
+import requests
+from requests import Response
+
+# 定義主程式函數
+def main():
+
+    # 定義API的URL（YOUBIKE台北市即時資訊）
+    url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+
+    # 輸出訊息到控制台
+    print("下載台北市YOUBIKE即時資訊")
+
+    # 使用requests發送GET請求
+    # `:requests.Response` 是類型註解，表示response變數的型態應該是Response物件
+    response:requests.Response = requests.get(url)
+    # 使用type()函數打印response物件的實際型態
+    print(type(response))
+
+    # 檢查HTTP狀態碼是否為200（200代表請求成功）
+    if response.status_code == 200:
+        # 呼叫.json()方法，將JSON格式的文本轉換為Python中的列表或字典
+        data = response.json()
+        # 輸出"下載成功"訊息
+        print("下載成功")
+        # 使用type()函數查看data的型態（通常是list列表）
+        print(type(data))
+        # 使用len()函數得到data列表中元素的個數
+        print(len(data))        
+        # 使用索引[0]取出data列表的第一個元素，並打印它
+        print(data[0])
+    else:  
+        # 如果狀態碼不是200，輸出"下載失敗"訊息
+        print("下載失敗")
+        # 打印HTTP狀態碼
+        print(response.status_code)
+
+# 判斷這個檔案是否直接被執行（而不是被導入其他檔案）
+if __name__ == '__main__':
+    # 執行主程式函數
+    main()
